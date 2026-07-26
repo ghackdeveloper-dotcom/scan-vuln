80f8368a5ed8c1c0d9ff13f2ae5fd4d3f9839067
G.HACK <g.hack.developer@gmail.com>
2026-07-26 23:51:31 +0100

    SCAN VULN LINUX.py

--- SCAN VULN LINUX.py
+++ SCAN VULN LINUX.py
@@ -1,89 +1,73 @@
+# SCAN LINUX
+#Produttore g.hack
+
 import time
 import subprocess
 
-def nmap():
-    print("[BENVENUTO NEL TUO ENUMERATORE DI VULN PERSONALE]")
-    time.sleep(2)
-    print("[INIZIAMO LO SCAN CON NMAP + IL CONTROLLO VERSIONE E SCAN DI OGNI SINGOLA PORTA UN CLASSICO]")
-    time.sleep(1)
-    print("[INSERISCI IP DEL TRAGET]")
-    ip = input()
-    time.sleep(1)
-    print("[ATTENDI...]")
-    try:
-        subprocess.run([f"nmap -sC -sV -p- {ip} | grep open >> analisi_macchina.txt"], shell=True, check=True)
-        print("[LE VERSIONI SONO STATE SALVATE IN: analisi_macchina.txt]")
-        time.sleep(1)
-        print("[PASSAGGIO A GOBUSTER...]")
-        time.sleep(2)
-    except subprocess.CalledProcessError:
-        print("[QUALCOSA CON NMAP E ANDATO STORTO CONTROLLA LA CONNESSIONE O INSERISCI TRAGET VALIDO]")
-        time.sleep(1)
-        print("[PASSAGGIO A GOBUSTER...]")
-        time.sleep(2)
-nmap()
 
-def gobuster():
-    print("[IL PASSAGGIO A GOBUSTER E AVVENUTO CON SUCCESSO]")
-    time.sleep(2)
-    print("[FORNISCI IL LINK DEL TRAGET ES: https://esempio.com]")
-    link = input()
-    print("[ATTENDI...]")
-    try:
-        subprocess.run([f"gobuster dir -u {link} -w /usr/share/dirb/wordlists/common.txt | grep Status >> analisi_macchina.txt"], shell=True, check=True)
-        print("\n[LE DIR SONO STATE SALVATE IN: analisi_macchina.txt]")
-        time.sleep(1)
-        print("[PASSAGGIO A FFUF...]")
-        time.sleep(2)
-    except subprocess.CalledProcessError:
-        print("[QUALCOSA CON GOBUSTER E ANDATO STORTO CONTROLLA CHE IL LINK SIA VALIDO O LA CONNSESSIONE FUNZIONI]")
-        time.sleep(1)
-        print("[PASSAGGIO A FFUF...]")
-        time.sleep(2)
-gobuster()
+class scan_linux():
 
-def ffuf():
-    print("[IL PASSAGGIO A FFUF E AVVENUTO CON SUCCESSO]")
-    time.sleep(2)
-    print("[FORNISCI IL LINK ES: https://esempio.com]")
-    link2 = input()
-    print("[ORA FORNISCI IL NOME + COM O IT ES: esempio.com]")
-    link3 = input()
-    print(f"[PRENDO I DATI NECCESSARI DA {link2} ATTENDI...]")
-    try:
-        subprocess.run([f"""curl -I -H "Host: xyz123random.{link3}" {link2} | grep Content-Length:"""], shell=True, check=True)
-        subprocess.run([f"""curl -I -H "Host: xyz123random.{link3}" {link2} | grep HTTP"""], shell=True, check=True)
-        time.sleep(1)
-        print("[SE IL NUMERO E PRESENTE DIGITARE -fs + IL NUMERO A TRE CIFRE DOPO Content-Length]")
-        time.sleep(1)
-        print("[SE IL NUMERO NON E PRESENTE DIGITARE -fc + IL NUMERO A TRE CIFRE DOPO HTTP]")
-        time.sleep(1)
-        print("[INSERISCI IL FLAG CHE VUOI USARE (-fs/-fc)]")
-        flag = input()
-        print("[ORA INSERISCI IL NUMERO]")
-        numero = input()
-        print("[INIZIALIZZAZIONE DI FFUF ATTENDI...]")
-        time.sleep(2)
+    def configurazione(self):
+        ip = input("INSERISCI IP:")
+        dominio = input("INSERISCI DOMINIO:")
+        self.nmap_gobuster_ffuf_feroxbuster(ip, dominio)
+        self.smb_anon(ip, dominio)
+
+    def nmap_gobuster_ffuf_feroxbuster(self, ip, dominio):
+        print("[SCAN NMAP COMPLETO O STANDARD SE COMPLETO DIGITA -p- SE STANDARD PREMI SOLO INVIO]")
+        opzione = input("MODALITA:")
+        print("[SCAN INIZIATO ATTENDI...]")
         try:
-            print("[SE INIZIA AD IMPAZZIRE PREMI ctrl+C VUOLDIRE CHE IL NUMERO NON E VALIDO]")
-            time.sleep(1)
-            subprocess.run([f"""ffuf -u {link2} -H "Host: FUZZ.{link3}" -w /usr/share/wordlists/subdomains.txt {flag} {numero}"""], shell=True, check=True)
-            print("[FFUF HA SCANALIZZATO CON SUCCESSO]")
-            time.sleep(1)
-            print("[TI TROVO ALTRI DATI SUL SERVER]")
-            try:
-                subprocess.run([f"""curl -I -H "Host: xyz123random.{link3}" {link2} >> analisi_macchina.txt"""], shell=True, check=True)
-                print("[I DATI SONO STATI SALVATI IN analisi_macchina.txt]")
-            except:
-                print("[QUALCOSA E ANDATO STORTO CONTROLLA CHE IL LINK SIA VALIDO O LA CONNESSIONE]")
-                time.sleep(1)
-                print("[ENUM FINITA]")
+            subprocess.run([f"nmap {opzione} {ip} >> nmap_scan.txt"], shell=True, check=True)
+            print("[SCAN NMAP CONCLUSO CON SUCCESSO!]")
+            time.sleep(0.5)
+            print("[OUTPUT DI NMAP SALVATO SULLA HOME IN nmap_scan.txt]")
+            with open ("nmap_scan.txt", "r") as f:
+                contenuto = f.read()
+            if "80/tcp" in contenuto or "443/tcp" in contenuto:
+                print("[UN SITO E STATO RILEVATO]")
+                percorso = input("FORNISCI IL PERCORSO DELLA WORDLIST PER GOBUSTER:")
+                percorso2 = input("FORNISCI IL PERCORSO DELLA WORDLIST PER FEROXBUSTER:")
+                percorso3 = input("FORNISCI IL PERCORSO DELLA WORDLIST PER FFUF:")
+                print("[SCAN CON GOBUSTER INIZIATO ATTENDI...]")
+                try:
+                    subprocess.run([f"gobuster dir -u http://{dominio} -w {percorso} -o gobuster_scan.txt"], shell=True, check=True)
+                    print("[OUTPUT DI GOBUSTER SALVATO SULLA HOME IN gobuster_scan.txt]")
+                except subprocess.CalledProcessError:
+                    print("[GOBUSTER DA PROBLEMI LO SOSTITUISCO CON FEROXBUSTER]")
+                    print("[SCAN CON FEROXBUSTER INIZIATO ATTENDI...]")
+                    subprocess.run([f"feroxbuster -u http://{dominio} -w {percorso2} -t 50 -o ferox_scan.txt"], shell=True, check=True)
+                except Exception as e:
+                    print("[SIA GOBUSTER CHE FEROXBUSTER DANNO PROBLEMI ANALISI SALTATA]")
+                print("[ESTRAGGO IL DATO PER LO SCAN DI FFUF ATTENDI...]")
+                try:
+                    subprocess.run([f"""curl -I -H "Host: xyz123random.{dominio}" http://{dominio} | grep Content-Length:"""], shell=True, check=True)
+                    print("[DATO PRESO CON SUCCESSO!]")
+                except subprocess.CalledProcessError:
+                    print("[NON E STATO POSSIBILE PRENDERE IL DATO NECESSARIO CONTROLLA LA CONNESSIONE O CORREGGI IL LINK]")
+                dato = input("[INSERISCI IL NUMERO DOPO Content-Length:]")
+                print("[PARTO CON FFUF]")
+                try:
+                    subprocess.run([f"""ffuf -u http://{dominio} -H "Host: FUZZ.{dominio}" -w {percorso3} -fs {dato}"""], shell=True, check=True)
+                    print("[FFUF HA TERMINATO CON SUCCESSO]")
+                except subprocess.CalledProcessError:
+                    print("[FFUF E ANDATO IN ERRORE]")
+            if "80/tcp" not in contenuto or "443/tcp" not in contenuto:
+                print("[NESSUN SITO RILEVATO]")
         except subprocess.CalledProcessError:
-            print("[QUALCOSA CON FFUF E ANDATO STORTO CONTROLLA CHE IL LINK SIA VALIDO O LA CONNESSIONE]")
-            time.sleep(1)
-            print("[ENUM FINITA]")
-    except subprocess.CalledProcessError:
-        print("[I DATI SONO INVALIDI O LA CONNESIONE E SALTATA]")
-        time.sleep(1)
-        print("[ENUM FINITA]")
-ffuf()
+            print("[SCAN NMAP FALLITO CONTROLLA LA CONNESSIONE]")
+        except Exception as e:
+            print(f"[ERRORE: {e}]")
+    
+    def smb_anon(self, ip, dominio):
+        print("[PROVO ACCESSO ANONIMO SU SMB]")
+        try:
+            subprocess.run(["smbclient "], shell=True, check=True)
+            print("[TENTATIVO DI CONNESSIONE ANONIMA RIUSCITO]")
+            print("[SCAN LINUX FINITO CON SUCCESSO!]")
+        except subprocess.CalledProcessError:
+            print("[TENTATIVO DI CONNESSIONE ANONIMA FALLITO]")
+            print("[SCAN LINUX FINITO CON SUCCESSO]")
+            
+scan = scan_linux()
+scan.configurazione()
