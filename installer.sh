cce9320f419deeee3cc79ef5986db0f7d59a3a49
G.HACK <g.hack.developer@gmail.com>
2026-09-20 16:59:16 +0100

    installer.sh
    The script installs various dependencies including nmap, gobuster, and several Python packages using pipx.

---
+++ installer.sh
@@ -0,0 +1,16 @@
+echo "INSTALLO TUTTE LE DIPENDENZE"
+
+sudo apt install nmap
+sudo apt install gobuster
+sudo apt install ffuf
+sudo apt install nxc
+sudo apt install feroxbuster
+sudo apt install samba
+sudo apt install openldap
+sudo apt install pipx
+pipx install certipy-ad
+pipx install bloodhound
+pipx install bloodyAD
+pipx install impacket
+
+echo "TUTTE LE DIPENDENZE SONO INSTALLATE"
